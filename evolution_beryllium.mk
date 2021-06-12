2d8c73266968f705d99465aa22b205833eb2d0d6
Cruzer1 <preetamswain143@gmail.com>
2021-06-12 12:24:42 +0000

    test

--- evolution_beryllium.mk
+++ evolution_beryllium.mk
@@ -7,16 +7,17 @@
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
 # Inherit some common STAG OS stuff.
-$(call inherit-product, vendor/stag/main.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := stag_beryllium
+PRODUCT_NAME := evolution_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
